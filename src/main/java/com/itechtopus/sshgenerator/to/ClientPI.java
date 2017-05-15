25a71f350df6a2628599864a4859d3d983b07a5f
Yerlan <Paintballworld@rambler.ru>
2017-05-15 21:48:45 +0600

    Improoved class

--- src/main/java/com/itechtopus/sshgenerator/to/ClientPI.java
+++ src/main/java/com/itechtopus/sshgenerator/to/ClientPI.java
@@ -19,4 +19,32 @@ public class ClientPI extends ModelParent {
   public Map<AddressType, ClientAddr> addresses = Util.newMap();
 
   public List<ClientPhone> phones = Util.newList();
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ClientPI clientPI = (ClientPI) o;
+
+    if (client != null ? !client.equals(clientPI.client) : clientPI.client != null) return false;
+    if (addresses != null ? !addresses.equals(clientPI.addresses) : clientPI.addresses != null) return false;
+    return phones != null ? phones.equals(clientPI.phones) : clientPI.phones == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = client != null ? client.hashCode() : 0;
+    result = 31 * result + (addresses != null ? addresses.hashCode() : 0);
+    result = 31 * result + (phones != null ? phones.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ClientPI{" + client.surname +
+        " " + client.name.charAt(0) +
+        ". " + client.patronymic.charAt(0)+
+        '}';
+  }
 }
