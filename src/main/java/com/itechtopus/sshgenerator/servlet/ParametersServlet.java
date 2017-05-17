6c9b49140feb8ead81ac5328c8517b4b4909714a
Yerlan <Paintballworld@rambler.ru>
2017-05-17 21:35:45 +0600

    Added parameters managing servlet

---
+++ src/main/java/com/itechtopus/sshgenerator/servlet/ParametersServlet.java
@@ -0,0 +1,55 @@
+package com.itechtopus.sshgenerator.servlet;
+
+import com.itechtopus.sshgenerator.storage.MainStorage;
+import com.itechtopus.sshgenerator.storage.Parameters;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ParametersServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.setAttribute("OUTPUT_SCHEDULER_PERIOD", Parameters.OUTPUT_SCHEDULER_PERIOD);
+    request.setAttribute("TRANSACTIONS_PER_ITERATION", Parameters.TRANSACTIONS_PER_ITERATION);
+    request.setAttribute("ACCOUNT_GENERATION_PERIOD", Parameters.ACCOUNT_GENERATION_PERIOD);
+    request.setAttribute("CLIENT_GENERATION_PERIOD", Parameters.CLIENT_GENERATION_PERIOD);
+    request.setAttribute("GENERATE_DUPLICATES", Parameters.GENERATE_DUPLICATES);
+    request.setAttribute("SCHEDULER_PERIOD", Parameters.SCHEDULER_PERIOD);
+    request.setAttribute("DUPLICATES_GENERATION_PERIOD", Parameters.DUPLICATES_GENERATION_PERIOD);
+    request.setAttribute("generatedClients", MainStorage.clientPIs.size());
+    request.setAttribute("generatedAccounts", MainStorage.accounts.size());
+    request.getRequestDispatcher("/WEB-INF/parameters.jsp").forward(request, response);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String value = request.getParameter("OUTPUT_SCHEDULER_PERIOD");
+    if (value != null && !value.isEmpty())
+      Parameters.OUTPUT_SCHEDULER_PERIOD = Integer.valueOf(value);
+    value = request.getParameter("TRANSACTIONS_PER_ITERATION");
+    if (value != null && !value.isEmpty())
+      Parameters.TRANSACTIONS_PER_ITERATION = Integer.valueOf(value);
+    value = request.getParameter("ACCOUNT_GENERATION_PERIOD");
+    if (value != null && !value.isEmpty())
+      Parameters.ACCOUNT_GENERATION_PERIOD = Integer.valueOf(value);
+    value = request.getParameter("CLIENT_GENERATION_PERIOD");
+    if (value != null && !value.isEmpty())
+      Parameters.CLIENT_GENERATION_PERIOD = Integer.valueOf(value);
+    value = request.getParameter("GENERATE_DUPLICATES");
+    if (value != null && !value.isEmpty())
+      Parameters.GENERATE_DUPLICATES = Boolean.valueOf(value);
+    value = request.getParameter("SCHEDULER_PERIOD");
+    if (value != null && !value.isEmpty())
+      Parameters.SCHEDULER_PERIOD = Integer.valueOf(value);
+    value = request.getParameter("DUPLICATES_GENERATION_PERIOD");
+    if (value != null && !value.isEmpty())
+      Parameters.DUPLICATES_GENERATION_PERIOD = Integer.valueOf(value);
+
+    doGet(request, response);
+
+  }
+}
---
+++ src/main/webapp/WEB-INF/parameters.jsp
@@ -0,0 +1,57 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yerlan
+  Date: 17/05/17
+  Time: 9:05 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Parameters</title>
+</head>
+<body>
+    <form action="parameters" method="post">
+        <p>Сгенерировано клиентов:<%= request.getAttribute("generatedClients")%>,
+            Сгенерировано счетов:<%= request.getAttribute("generatedAccounts")%></p>
+        <p>
+            OUTPUT_SHEDULER_PERIOD
+            <input type="number" name = "OUTPUT_SCHEDULER_PERIOD" value = "<%= request.getAttribute("OUTPUT_SCHEDULER_PERIOD")%>"
+            title="Период запуска шедула по выводу сгенеренных данных в файлы в мс."/>
+        </p>
+        <p>
+            TRANSACTIONS_PER_ITERATION
+            <input type="number" name = "TRANSACTIONS_PER_ITERATION" value = "<%= request.getAttribute("TRANSACTIONS_PER_ITERATION")%>"
+            title="Количество транзакций, генерируемых за одну итерацию"/>
+        </p>
+        <p>
+            ACCOUNT_GENERATION_PERIOD
+            <input type="number" name = "ACCOUNT_GENERATION_PERIOD" value = "<%= request.getAttribute("ACCOUNT_GENERATION_PERIOD")%>"
+            title="Количество итераций между генерациями нового счета"/>
+        </p>
+        <p>
+            CLIENT_GENERATION_PERIOD
+            <input type="number" name = "CLIENT_GENERATION_PERIOD" value = "<%= request.getAttribute("CLIENT_GENERATION_PERIOD")%>"
+            title="Количество итераций между генерациями нового клиента"/>
+        </p>
+        <p>
+            GENERATE_DUPLICATES
+            <input type="text" name = "GENERATE_DUPLICATES" value = "<%= request.getAttribute("GENERATE_DUPLICATES")%>"
+            title="Генерировать дубликаты?"/>
+        </p>
+        <p>
+            DUPLICATES_GENERATION_PERIOD
+            <input type="number" name = "DUPLICATES_GENERATION_PERIOD" value = "<%= request.getAttribute("DUPLICATES_GENERATION_PERIOD")%>"
+            title="Количество генераций между генерацией дубликата"/>
+        </p>
+        <p>
+            SCHEDULER_PERIOD
+            <input type="number" name = "SCHEDULER_PERIOD" value = "<%= request.getAttribute("SCHEDULER_PERIOD")%>"
+            title="Время в милисекундах между итерациями для генераций сущностей"/>
+        </p>
+        <p>
+            <input type="submit" value="Send"/>
+        </p>
+    </form>
+</body>
+</html>
