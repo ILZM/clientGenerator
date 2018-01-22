ef6adeafa4c185b2bda3ac6c1e6b2e7e9bdcb737
yakzhanov <yakzhanov@greet-go.com>
2018-01-22 15:45:44 +0600

    added elapsed time printing

--- src/main/java/com/itechtopus/sshgenerator/static_tests/Main.java
+++ src/main/java/com/itechtopus/sshgenerator/static_tests/Main.java
@@ -1,5 +1,10 @@
 package com.itechtopus.sshgenerator.static_tests;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import com.itechtopus.sshgenerator.generator.AllInfoGenerator;
 import com.itechtopus.sshgenerator.sheduler.GeneratorScheduler;
 import com.itechtopus.sshgenerator.sheduler.OutputScheduler;
@@ -10,9 +15,13 @@ public class Main {
 
   public static void main(String[] args) throws InterruptedException {
 
+    Long startTime = System.currentTimeMillis();
+
     Thread counter = new Thread(() -> {
       while (!isInterrupted) {
-        System.err.print("\r Количество клиентов: " + AllInfoGenerator.get().clientPIS.size() + "" +
+        Long elapsed = System.currentTimeMillis() - startTime;
+        System.err.print("\r" + getStrRepresentationOfTime(elapsed) +
+          " Количество клиентов: " + AllInfoGenerator.get().clientPIS.size() + "" +
           " Количество счетов: " + AllInfoGenerator.get().accountMap.size());
         try {
           Thread.sleep(500);
@@ -35,6 +44,24 @@ public class Main {
 
     counter.join();
 
+  }
+
+  private static String getStrRepresentationOfTime(long elapsed) {
+    List<String> line = new ArrayList<>();
+    LinkedHashMap<Integer, String> timeMap = new LinkedHashMap<>();
+    timeMap.put(1000 * 60 * 60, " ч");
+    timeMap.put(1000 * 60, " м");
+    timeMap.put(1000, " c");
+//    timeMap.put(1, " мc");
+    for (Map.Entry<Integer, String> pair : timeMap.entrySet()) {
+      if (elapsed > pair.getKey()) {
+        int power = (int)(elapsed / pair.getKey());
+        line.add(power + pair.getValue());
+        elapsed %= pair.getKey();
+      }
+    }
+    String elapsedStr = line.stream().collect(Collectors.joining(" "));
+    return elapsedStr;
   }
 
 }
